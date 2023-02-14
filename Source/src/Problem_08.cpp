dbf7685029dfe0df4f8a8559c3aaf55eb16d6122
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-02-14 10:20:32 +0530

    Problem_08

---
+++ Source/src/Problem_08.cpp
@@ -0,0 +1,101 @@
+
+#include<iostream>
+#include<vector>
+#include <string.h>
+
+std::vector<int>  Find_13_AdjacentNumber( char num [] , int Seriel) 
+{
+  std::vector<int> SurroundedNumbers;
+  int End_Pos = strlen(num)-1;
+
+  //When seriel is starting Between 0 to 6
+  if( Seriel < 6)
+  {
+        int start = 0;
+        int end = 12 ;
+        for(int i = start ; i<= end ;i++ )
+        {
+          int Value = num[i] - '0';
+          SurroundedNumbers.push_back(Value);
+        }
+        return SurroundedNumbers;
+  }
+
+  //When Number is ending between the 6 less than number number length  and the end 
+  if( Seriel+6 > End_Pos)
+  {
+      int start = End_Pos-12 ;
+      int end = End_Pos;
+      for(int i = start ; i<= end ;i++ )
+        {
+          int Value = num[i] - '0';
+          SurroundedNumbers.push_back(Value);
+        }
+        return SurroundedNumbers;
+
+  }
+
+  // when the number has Available surrounded 13 digit both 6 on the left and 6 on the right 
+  for(int i = Seriel-6  ; i<=Seriel+6 ;i++)
+  {
+    int Value = num[i] - '0';
+    SurroundedNumbers.push_back(Value);
+  }
+  
+  return SurroundedNumbers;
+}
+
+
+
+
+char Numbers [] = "73167176531330624919225119674426574742355349194934"
+                "96983520312774506326239578318016984801869478851843"
+                "85861560789112949495459501737958331952853208805511"
+                "12540698747158523863050715693290963295227443043557"
+                "66896648950445244523161731856403098711121722383113"
+                "62229893423380308135336276614282806444486645238749"
+                "30358907296290491560440772390713810515859307960866"
+                "70172427121883998797908792274921901699720888093776"
+                "65727333001053367881220235421809751254540594752243"
+                "52584907711670556013604839586446706324415722155397"
+                "53697817977846174064955149290862569321978468622482"
+                "83972241375657056057490261407972968652414535100474"
+                "82166370484403199890008895243450658541227588666881"
+                "16427171479924442928230863465674813919123162824586"
+                "17866458359124566529476545682848912883142607690042"
+                "24219022671055626321111109370544217506941658960408"
+                "07198403850962455444362981230987879927244284909188"
+                "84580156166097919133875499200524063689912560717606"
+                "05886116467109405077541002256983155200055935729725"
+                "71636269561882670428252483600823257530420752963450";
+
+
+int main(int argc, char const *argv[])
+{
+
+
+long long int  LargestProduct =1;
+std::vector<int> LargestSurroundedNumbers ;
+int Count=0;
+for (int i = 6; i < strlen(Numbers)-6 ; i++)
+{
+  Count++;
+  long long int ProductSurroundedNumer =1;
+  std::vector<int> SurroundedNumber = Find_13_AdjacentNumber(Numbers ,i);
+  for (int  j = 0; j < SurroundedNumber.size() ; j++)
+  {
+    
+    ProductSurroundedNumer*= SurroundedNumber[j];
+    //printf("%d \n",ProductSurroundedNumer);
+  }
+  if(ProductSurroundedNumer > LargestProduct)
+  {
+    LargestProduct =ProductSurroundedNumer;
+    LargestSurroundedNumbers.assign(SurroundedNumber.begin(), SurroundedNumber.end());
+
+  }
+}
+
+
+    return 0;
+}
